7a8733875332ecc1910854bbfbae9787ed3f39db
arnab pratim saha <arnabpratimsaha@gmail.com>
2022-04-15 14:30:03 +0530

    fixed bug for vercel deploy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-const express=require('express');
+import express from 'express'
 const app=express();
 
 const PORT=9000;
-app.get('/',(req,res)=>{
-    res.status(200).json('hehe')
-})
 
+app.get('/',(req,res)=>{
+    res.status(200).json('hehe');
+})
 app.listen(PORT,()=>console.log(`Server listening on Port ${PORT}`));
